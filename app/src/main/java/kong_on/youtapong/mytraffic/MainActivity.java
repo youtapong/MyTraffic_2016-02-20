dea8a8e82d4e5b7020558edafe6e9e294d5a3eec
youtapong <youtapong.gmail.com>
2016-02-20 15:07:14 +0700

    Finish 2016-02-20

--- app/src/main/java/kong_on/youtapong/mytraffic/MainActivity.java
+++ app/src/main/java/kong_on/youtapong/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package kong_on.youtapong.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,10 +24,31 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidgt();
 
+        // Button Controller
+        buttonController();
+
     }//Main Method
 
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.bird_maxican);
+                mediaPlayer.start();
+
+                //web view
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/9lN_tL6jvwo"));
+                startActivity(intent);
+            }
+        });
+
+    }
+
     private void bindWidgt() {
-        trafficListView = findViewById(R.id.listView);
+        trafficListView = (ListView) findViewById(R.id.listView);
         aboutMeButton = (Button) findViewById(R.id.button);
     }
 } // Main Class
