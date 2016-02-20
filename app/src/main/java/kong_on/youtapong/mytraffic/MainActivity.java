91cf55f97c09cd02e688057fb7ef1e0eeaaf88e4
youtapong <youtapong.gmail.com>
2016-02-20 14:02:00 +0700

    Explicit เสร็จแล้ว

--- app/src/main/java/kong_on/youtapong/mytraffic/MainActivity.java
+++ app/src/main/java/kong_on/youtapong/mytraffic/MainActivity.java
@@ -2,12 +2,19 @@ package kong_on.youtapong.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /// Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }//Main Method
+} // Main Class
